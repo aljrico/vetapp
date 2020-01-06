a4f9e6ceb6832cdf9baad1dfe08d1d4e88821116
aljrico <aljrico@gmail.com>
2020-01-06 12:21:28 +0000

    clean all temporary files when closing

---
+++ R/mod_clean_up.R
@@ -0,0 +1,20 @@
+# Module Server
+    
+#' @rdname mod_clean_up
+#' @export
+#' @keywords internal
+    
+mod_clean_up_server <- function(input, output, session){
+  cancel.onSessionEnded <- session$onSessionEnded(function() {
+    temporary_files <- list.files(path = 'tmp/', full.names = TRUE, recursive = TRUE)
+    file.remove(temporary_files)
+    message('Temporary Files Removed Successfully')
+  })
+}
+    
+## To be copied in the UI
+# mod_clean_up_ui("clean_up_ui_1")
+    
+## To be copied in the server
+# callModule(mod_clean_up_server, "clean_up_ui_1")
+ 
